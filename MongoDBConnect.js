75510748b2d8283894ed5f842b9ecc86dc6f2efd
Supapong Putthayangkura <bigsupp@gmail.com>
2020-05-03 03:15:50 +0700

    add default options

--- MongoDBConnect.js
+++ MongoDBConnect.js
@@ -5,7 +5,16 @@ const MONGO_URI = process.env.MONGO_URI
 let isConnectedBefore = false;
 
 module.exports = (options) => {
-  mongoose.connect(MONGO_URI, options);
+  const default_options = {
+    "useNewUrlParser": true,
+    "useCreateIndex": true,
+    "auto_reconnect": true,
+    "reconnectTries": 180,
+    "reconnectInterval": 1000,
+    "connectTimeoutMS": 1000,
+    "socketTimeoutMS": 60000
+  }
+  mongoose.connect(MONGO_URI, options || default_options);
 }
 
 mongoose.connection.on('error', () => {
